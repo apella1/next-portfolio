92220ae34c2b47b993defb6f7de98d2c1d8e424d
apella1 <johncollons5@gmail.com>
2023-07-15 12:16:20 +0300

    Add first navbar iteration

---
+++ app/posts/[slug]/page.tsx
@@ -0,0 +1,7 @@
+export default function Post() {
+  return (
+    <section>
+      <h1>Post</h1>
+    </section>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { useState } from "react";
+import { DiGithubBadge } from "react-icons/di";
+import { FiLinkedin } from "react-icons/fi";
+import { RiCloseLine, RiMenuLine } from "react-icons/ri";
+
+const Navbar = () => {
+  const Menu = () => (
+    <>
+      <a
+        href="#home"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-4 rounded-md"
+      >
+        Home
+      </a>
+      <a
+        href="#about"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-4 rounded-md"
+      >
+        About
+      </a>
+      <a
+        href="#skills"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-4 rounded-md"
+      >
+        Skills
+      </a>
+      <a
+        href="#projects"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-4 rounded-md"
+      >
+        Projects
+      </a>
+      <a
+        href="#contact"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-4 rounded-md"
+      >
+        Contact
+      </a>
+      <a
+        href="https://github.com/apella1"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-3 rounded-2xl flex items-center space-x-2 border border-black p-4"
+      >
+        <DiGithubBadge className="text-2xl" />
+        <p>Github</p>
+      </a>
+
+      <a
+        href="https://linkedin.com/in/john-apella"
+        className="hover:bg-[#032cfc] hover:text-white py-2 px-3 rounded-2xl flex items-center space-x-2 border border-black p-4"
+      >
+        <FiLinkedin />
+        <p>LinkedIn</p>
+      </a>
+    </>
+  );
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const toggleNavbar = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+  return (
+    <nav className="flex items-center justify-between px-4 2xl:px-52 md:px-16 lg:px-28 py-4">
+      <section>
+        <p className="text-2xl">John Apella</p>
+      </section>
+      {/* menu */}
+      <section>
+        <div className="hidden md:flex md:items-center md:space-x-4">
+          <Menu />
+        </div>
+        {/* mobile menu icons */}
+        <div className="z-10 inline-block md:hidden">
+          {isNavOpen ? (
+            <RiCloseLine color="white" size={30} onClick={toggleNavbar} />
+          ) : (
+            <RiMenuLine color="white" size={30} onClick={toggleNavbar} />
+          )}
+        </div>
+        {/* mobile menu */}
+        <div
+          className={
+            !isNavOpen
+              ? "hidden"
+              : "flex absolute inset-0 w-full h-screen z-{1} flex-col items-center justify-center bg-[#23272F] space-y-2"
+          }
+        >
+          <Menu />
+        </div>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;

c49396ea96b1c9a748aa9d4a15264cc33e57704e
apella1 <johncollons5@gmail.com>
2023-07-28 08:44:59 +0300

    Implement skills section

--- components/skills/Skills.tsx
+++ components/skills/Skills.tsx
@@ -1,7 +1,265 @@
+import Image from "next/image";
+
 const Skills = () => {
   return (
-    <section className="min-h-screen overflow-auto px-4 sm:px-16 xl:px-28 2xl:px-52 py-8 flex flex-col">
-      Skills
+    <section className="min-h-screen overflow-auto px-4 sm:px-16 xl:px-28 2xl:px-52 py-8 2xl:py-12 flex flex-col">
+      <h1 className="text-4xl text-green-700 font-extrabold leading-9 tracking-tight sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 pb-12">
+        Skills
+      </h1>
+      <div className="pb-12">
+        <p className="flex gap-2 flex-wrap">
+          <a
+            href="https://www.typescriptlang.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/typescript-colored.svg"
+              width="36"
+              height="36"
+              alt="TypeScript"
+            />
+          </a>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/javascript-colored.svg"
+              width="36"
+              height="36"
+              alt="JavaScript"
+            />
+          </a>
+          <a
+            href="https://www.oracle.com/java/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/java-colored.svg"
+              width="36"
+              height="36"
+              alt="Java"
+            />
+          </a>
+          <a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/react-colored.svg"
+              width="36"
+              height="36"
+              alt="React"
+            />
+          </a>
+          <a href="https://nextjs.org/docs" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nextjs-colored.svg"
+              width="36"
+              height="36"
+              alt="NextJs"
+            />
+          </a>
+          <a href="https://redux.js.org/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/redux-colored.svg"
+              width="36"
+              height="36"
+              alt="Redux"
+            />
+          </a>
+          <a href="https://tailwindcss.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/tailwindcss-colored.svg"
+              width="36"
+              height="36"
+              alt="TailwindCSS"
+            />
+          </a>
+          <a href="https://vitejs.dev/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/vite-colored.svg"
+              width="36"
+              height="36"
+              alt="Vite"
+            />
+          </a>
+          <a href="https://chakra-ui.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/chakra-colored.svg"
+              width="36"
+              height="36"
+              alt="Chakra UI"
+            />
+          </a>
+          <a
+            href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/html5-colored.svg"
+              width="36"
+              height="36"
+              alt="HTML5"
+            />
+          </a>
+          <a
+            href="https://www.w3.org/TR/CSS/#css"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/css3-colored.svg"
+              width="36"
+              height="36"
+              alt="CSS3"
+            />
+          </a>
+          <a href="https://getbootstrap.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/bootstrap-colored.svg"
+              width="36"
+              height="36"
+              alt="Bootstrap"
+            />
+          </a>
+          <a href="https://nodejs.org/en/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/nodejs-colored.svg"
+              width="36"
+              height="36"
+              alt="NodeJS"
+            />
+          </a>
+          <a href="https://expressjs.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/express-colored.svg"
+              width="36"
+              height="36"
+              alt="Express"
+            />
+          </a>
+          <a href="https://graphql.org/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/graphql-colored.svg"
+              width="36"
+              height="36"
+              alt="GraphQL"
+            />
+          </a>
+          <a href="https://www.mongodb.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/mongodb-colored.svg"
+              width="36"
+              height="36"
+              alt="MongoDB"
+            />
+          </a>
+          <a
+            href="https://www.postgresql.org/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/postgresql-colored.svg"
+              width="36"
+              height="36"
+              alt="PostgreSQL"
+            />
+          </a>
+          <a
+            href="https://firebase.google.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/firebase-colored.svg"
+              width="36"
+              height="36"
+              alt="Firebase"
+            />
+          </a>
+          <a href="https://www.heroku.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/heroku-colored.svg"
+              width="36"
+              height="36"
+              alt="Heroku"
+            />
+          </a>
+          <a href="https://aws.amazon.com" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/aws-colored.svg"
+              width="36"
+              height="36"
+              alt="Amazon Web Services"
+            />
+          </a>
+          <a href="https://www.docker.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/docker-colored.svg"
+              width="36"
+              height="36"
+              alt="Docker"
+            />
+          </a>
+          <a href="https://www.linux.org" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/linux-colored.svg"
+              width="36"
+              height="36"
+              alt="Linux"
+            />
+          </a>
+          <a href="https://www.figma.com/" target="_blank" rel="noreferrer">
+            <Image
+              src="https://raw.githubusercontent.com/danielcranney/readme-generator/main/public/icons/skills/figma-colored.svg"
+              width="36"
+              height="36"
+              alt="Figma"
+            />
+          </a>
+        </p>
+      </div>
+      <section className="flex flex-col space-y-6 lg:space-y-0 lg:flex-row lg:space-x-8">
+        <div className="flex flex-col space-y-6 lg:basis-1/2">
+          <p className="xl:text-2xl font-semibold self-center">Programming Languages</p>
+          <div className="w-[400px] self-center xl:w-full">
+            <figure>
+              <embed src="https://wakatime.com/share/@apella/f0e37590-b3d9-449f-be5d-957ea6789b5d.svg"></embed>
+            </figure>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-6 lg:basis-1/2">
+          <p className="xl:text-2xl font-semibold self-center">Code Editors/IDEs</p>
+          <div className="w-[400px] self-center xl:w-full">
+            <figure>
+              <embed src="https://wakatime.com/share/@apella/8341732b-3c13-4ead-9332-1f5e36c54f41.svg"></embed>
+            </figure>
+          </div>
+        </div>
+      </section>
+      <section className="flex flex-col space-y-6 lg:space-y-0 lg:flex-row lg:space-x-8 pt-8 xl:self-center">
+        <div className="flex flex-col space-y-6 lg:basis-1/2">
+          <p className="xl:text-2xl font-semibold text-center">
+            Operating Systems
+          </p>
+          <div className="w-[400px] self-center xl:w-[400px]">
+            <figure>
+              <embed src="https://wakatime.com/share/@apella/b456c7f1-e1c4-43a5-8ece-a04ff7c573bb.svg"></embed>
+            </figure>
+          </div>
+        </div>
+        <div className="flex flex-col space-y-6 lg:basis-1/2">
+          <p className="xl:text-2xl font-semibold text-center">Coding Activity</p>
+          <div className="w-[400px] self-center xl:w-[400px]">
+            <figure>
+              <embed src="https://wakatime.com/share/@apella/99b28e19-3765-4df8-b982-35fadce51143.svg"></embed>
+            </figure>
+          </div>
+        </div>
+      </section>
     </section>
   );
 };

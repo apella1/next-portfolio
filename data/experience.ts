a02e078ec446e14d7b96829270987989068f5cc4
apella1 <johncollons5@gmail.com>
2023-11-08 19:42:57 +0300

    Fix ABC action point grammar

--- data/experience.ts
+++ data/experience.ts
@@ -8,7 +8,7 @@ export const experiences: ExperienceProps[] = [
     startDate: "2023-04-01",
     endDate: "Present",
     achievements: [
-      "Acted as a key contributor in the development of the pioneering Africa Women Of Web3 initiative platform.",
+      "Acted as a key contributor to the development of the pioneering Africa Women Of Web3 initiative platform.",
       "Led the implementation of the redesign of the Afyalife Foundation's website, enhancing its visual appeal, user experience, and overall performance.",
       "Successfully migrated Cryptip website's code base from JavaScript to TypeScript, improving code maintainability and reducing the likelihood of runtime errors.",
     ],

527e2d330515de986124314dda95916083663dea
apella1 <johncollons5@gmail.com>
2024-07-19 16:32:45 +0300

    fix: revert ThemeSwitcher component state vars from hook to component

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -1,8 +1,32 @@
-import { useTheme } from "@/hooks/useTheme";
+"use client";
+
+import { useEffect, useState } from "react";
 import { BsMoonFill, BsSun } from "react-icons/bs";
 
-const ThemeSwitcher: React.FC = () => {
-  const { isDarkMode, toggleTheme } = useTheme();
+const ThemeSwitcher = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const isDarkModePreferred = localStorage.getItem("darkMode") === "true";
+    setIsDarkMode(isDarkModePreferred);
+  }, []);
+
+  const toggleTheme = () => {
+    const newTheme = !isDarkMode;
+    setIsDarkMode(newTheme);
+    localStorage.setItem("darkMode", newTheme.toString());
+    applyTheme(newTheme);
+  };
+
+  const applyTheme = (darkMode: boolean) => {
+    const theme = darkMode ? "dark" : "light";
+    document.documentElement.setAttribute("data-theme", theme);
+  };
+
+  useEffect(() => {
+    applyTheme(isDarkMode);
+  }, [isDarkMode]);
+
   return (
     <button onClick={toggleTheme}>
       {isDarkMode ? (

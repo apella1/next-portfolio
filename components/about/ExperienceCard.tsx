3a846616c1bc893cccb753ac42dbd142b3eeb620
apella1 <johncollons5@gmail.com>
2024-10-10 17:30:44 +0300

    feat: update experience card

--- components/about/ExperienceCard.tsx
+++ components/about/ExperienceCard.tsx
@@ -7,50 +7,9 @@ export interface ExperienceProps {
   achievements: string[];
 }
 
-const calculateDuration = (startDate: string, endDate: string) => {
-  const start = new Date(startDate);
-  const end =
-    endDate.trim().toLowerCase() === "present" ? new Date() : new Date(endDate);
-
-  if (end < start) return "0 months";
-
-  const diffInMs = end.getTime() - start.getTime();
-
-  const diffInDays = Math.floor(diffInMs / (1000 * 60 * 60 * 24));
-
-  let years = end.getFullYear() - start.getFullYear();
-  let months = end.getMonth() - start.getMonth();
-  let days = end.getDate() - start.getDate();
-
-  if (days < 0) {
-    months -= 1;
-    const prevMonth = new Date(end.getFullYear(), end.getMonth(), 0);
-    days += prevMonth.getDate();
-  }
-
-  if (months < 0) {
-    years -= 1;
-    months += 12;
-  }
-
-  let duration = "";
-  if (years > 0) {
-    duration += `${years} year${years > 1 ? "s" : ""} `;
-  }
-  if (months > 0) {
-    duration += `${months} month${months > 1 ? "s" : ""} `;
-  }
-  if (days > 0 || duration === "") {
-    duration += `${days} day${days > 1 ? "s" : ""}`;
-  }
-
-  return duration.trim();
-};
-
 const ExperienceCard = ({ experience }: { experience: ExperienceProps }) => {
   const { company, companyUrl, role, startDate, endDate, achievements } =
     experience;
-  const duration = calculateDuration(startDate, endDate);
 
   return (
     <div className="rounded-lg shadow-md p-6 flex flex-col space-y-3">
@@ -69,7 +28,6 @@ const ExperienceCard = ({ experience }: { experience: ExperienceProps }) => {
             {endDate.charAt(0).toUpperCase() + endDate.slice(1)}
           </p>
         </div>
-        <p className="">Duration: {duration}</p>
       </div>
       {achievements.length > 0 && (
         <div className="mt-4">
--- data/experience.ts
+++ data/experience.ts
@@ -17,12 +17,4 @@ export const experiences: ExperienceProps[] = [
     endDate: "2024-05-31",
     achievements: [],
   },
-  {
-    company: "Zima",
-    companyUrl: "",
-    role: "Software Engineer",
-    startDate: "2022-08-01",
-    endDate: "2023-04-01",
-    achievements: [],
-  },
 ];

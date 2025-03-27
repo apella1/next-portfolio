ad9aa6c8c951bbf541a9925e4de2112f68601178
apella1 <johncollons5@gmail.com>
2025-03-27 15:26:14 +0300

    feat: update title  metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "John Apella | Software Engineer | TypeScript Wizard",
+  title: "John Apella | Fullstack Software Engineer",
   description:
     "Experienced Software Engineer specializing in React, Next.js, TypeScript, Kotlin, Java, and Spring Boot. I excel in building modern web applications within the JVM ecosystem, delivering seamless user experiences and high-quality code. Passionate about leveraging the latest technologies to drive project success and foster collaboration. Explore my portfolio to see how I can help elevate your next project.",
 };

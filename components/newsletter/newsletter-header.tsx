852bda31c1c3212780ffbf5e94cea620693b3e01
John Apella <johncollons5@gmail.com>
2025-10-19 11:56:46 +0300

    fix: update newsletter prose styling

--- components/newsletter/newsletter-header.tsx
+++ components/newsletter/newsletter-header.tsx
@@ -5,15 +5,15 @@ import EmailOctopusNewsletterForm from "@/components/email-octopus-newsletter-fo
 const NewsletterHeader = () => {
   return (
     <div className="space-y-6">
-      <div className="space-y-2">
+      <div className="space-y-2 sm:space-y-6">
         <h1 className="text-2xl font-semibold tracking-tighter sm:text-4xl">
           Cloud Drills Newsletter
         </h1>
-        <p className="max-w-7xl">
+        <p className="max-w-7xl text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
           I started this newsletter to master cloud fundamentals through
           hands-on practice while sharing what I learn with fellow builders.
           Each week, I share a practical lab every Wednesday, going through the
-          building block for working on more complex projects. It's my way of
+          building blocks for working on more complex projects. It's my way of
           learning by doing and connecting with others on the same journey
           toward cloud mastery. Enter your email below to join me every week.
         </p>
